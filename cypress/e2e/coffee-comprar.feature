387ee45d9de5bcf5e991ce6bca3e6f766a6f6e7e
BrunofCarvalhoo <bcf2@cesar.school>
2026-01-17 20:56:26 -0300

    melhorias

--- cypress/e2e/coffee-comprar.feature
+++ cypress/e2e/coffee-comprar.feature
@@ -5,10 +5,10 @@ Funcionalidade: Comprar cafe
 
     Cenario: Comprar 3 cafes diferentes 
         Dado que o usuario esteja na pagina do coffee cart
-        Quando adicionar 3 cafes diferentes ao carrinho
-        E validar a aparicao da oferta
-        E aceitar a oferta
-        E validar os itens estao presentes no carrinho com os valores certos
-        E remover 1 item do carrinho
+        Quando ele adiciona 3 cafes diferentes ao carrinho
+        E valida a aparicao da oferta
+        E aceita a oferta
+        E valida que os itens estao presentes no carrinho com os valores certos
+        E remove 1 item do carrinho
         Entao preenche os dados de pagamento
-        E validar a mensagem de sucesso na compra
+        E valida a mensagem de sucesso na compra
--- cypress/support/step-definitions/coffee.cy.js
+++ cypress/support/step-definitions/coffee.cy.js
@@ -6,26 +6,26 @@ Given('que o usuario esteja na pagina do coffee cart', () =>{
     cy.visit('/')
 })
 
-When('adicionar 3 cafes diferentes ao carrinho', () =>{
+When('ele adiciona 3 cafes diferentes ao carrinho', () =>{
     CoffeeMenuPage.adicionarCafes()
 })
-And('validar a aparicao da oferta', () =>{
+And('valida a aparicao da oferta', () =>{
     CoffeeMenuPage.validarOfertaCafe()
 })
-And('aceitar a oferta', () => {
+And('aceita a oferta', () => {
     CoffeeMenuPage.aceitarOfertaCafe()
 })
-And('validar os itens estao presentes no carrinho com os valores certos', () =>{
+And('valida que os itens estao presentes no carrinho com os valores certos', () =>{
     CoffeeCartPage.cartPage()
     CoffeeCartPage.validarItensCart()
 })
-And('remover 1 item do carrinho', () => {
+And('remove 1 item do carrinho', () => {
     CoffeeCartPage.removerItemCart('Americano')
 })
-And('preenche os dados de pagamento', () =>{
+Then('preenche os dados de pagamento', () =>{
     CoffeePagamentoPage.irPagamento()
     CoffeePagamentoPage.preencherDadosPagamento()
 })
-Then('validar a mensagem de sucesso na compra', () =>{
+And('valida a mensagem de sucesso na compra', () =>{
     CoffeePagamentoPage.validarMensagemSucesso()
 })
